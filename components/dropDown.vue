2d767e9426707fdc4fbc5300036e8976d8821d56
= <=>
2020-07-14 21:43:39 +0300

    init project

---
+++ components/dropDown.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dropDown">
+    <slot name="mainButton">
+      <div class="dropDown__mainButton">{{ mainButtonText }}</div>
+    </slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class DropDown extends Vue {
+  @Prop() parent?: boolean = false;
+  @Prop() siblings?: boolean = false;
+  @Prop() mainButtonText: string = "Options";
+
+  created() {
+    if (this?.parent !== undefined) {
+      console.log("is Parent");
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dropDown {
+  .dropDown__mainButton {
+    color: red;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>Test Nuxt</h3>
+    <dropDown parent siblings />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import dropDown from "@/components/dropDown.vue";
+
+@Component({
+  components: {
+    dropDown
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+</style>
