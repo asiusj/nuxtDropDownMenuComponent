b4061e176bd6479d3c6de0866cc51006fcbc4fdc
= <=>
2020-07-15 21:57:42 +0300

    add news page

---
+++ pages/news.vue
@@ -0,0 +1,9 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col class="text-center p-4">
+                <h4>Some other page</h4>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
